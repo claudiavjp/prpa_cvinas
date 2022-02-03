e97b498d2eb4a820aec0d6745650c2c228ca7212
CLAUDIA VIÑAS JÁÑEZ <cvinas@meralloz4.fdi.ucm.es>
2022-02-03 20:07:53 +0100

    procesos

--- 0_procesos.py
+++ 0_procesos.py
@@ -1,9 +1,11 @@
 from time import sleep
 from random import random
+
 """
 Documentación multiprocessing
 https://docs.python.org/3.6/library/multiprocessing.html
 """
+
 from multiprocessing import Process
 
 def f():
